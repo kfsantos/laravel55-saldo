ca52e7add3f9fe4f60034a03bcd396f718053706
klinger dos santos <kfsantos@gmail.com>
2019-03-13 16:43:13 -0400

    Perfil de Usuário

---
+++ app/Http/Controllers/admin/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function profile(){
+        return view('site.profile.profile');
+    }
+
+    public function profileUpdate(Request $request){
+        dd($request->all());
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'adm
     Route::get('/', 'AdminController@index')->name('admin.home');
 });
 
-
+Route::post('atualizar-perfil', 'Admin\UserController@profileUpdate')->name('profile.update')->middleware('auth');
+Route::get('meu-perfil', 'Admin\UserController@profile')->name('profile')->middleware('auth');
 
 Route::get('/', 'Site\SiteController@index')->name('home');
 
